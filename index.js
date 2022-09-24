1411b5cadb915f7b725d1b4d52443ee54182e81a
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-24 12:08:12 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json()) //the built-in json method returns the response as a POJO(plain old JavaScript object) and it is another promise
+    .then(booksData => renderBooks(booksData)); //pass the returned object to the renderBooks function
 }
 
 function renderBooks(books) {
@@ -12,6 +14,8 @@ function renderBooks(books) {
   });
 }
 
+//on page load
+//listen to events after the HTML and CSS have parsed
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
